74bd1b826f76a3063964c934955870b7c76fadcf
Jackson Millard <jacksonmillard@Jacksons-MacBook-Pro.local>
2023-03-21 15:57:03 -0400

    clean up csr.conf and docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ if [ $(grep -e 'GRAPH_EXP_HTTPS_CONNECTION' ./packages/graph-explorer/.env | cut
         cd /graph-explorer/packages/graph-explorer-proxy-server/cert-info/
         sed -i "21s/$/ $HOST:*/" csr.conf
         sed -i "8s/$/ $HOST:*/" cert.conf
-        openssl req -x509 -sha256 -days 356 -nodes -newkey rsa:2048 -subj "/CN=Amazon Neptune/C=US/L=Seattle" -keyout rootCA.key -out rootCA.crt
+        openssl req -x509 -sha256 -days 356 -nodes -newkey rsa:2048 -subj "/CN=Graph Explorer/C=US/L=Seattle" -keyout rootCA.key -out rootCA.crt
         openssl genrsa -out ./server.key 2048
         openssl req -new -key ./server.key -out ./server.csr -config ./csr.conf
         openssl x509 -req -in ./server.csr -CA ./rootCA.crt -CAkey ./rootCA.key -CAcreateserial -out ./server.crt -days 365 -sha256 -extfile ./cert.conf
