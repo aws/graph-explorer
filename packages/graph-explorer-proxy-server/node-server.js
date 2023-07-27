04dd028be437d3b4fbbf8d94cffd2290ee1d8302
cubeddu <juan.cubeddu@gmail.com>
2023-07-27 09:29:29 -0500

    Added DS_Store to get gitignore, formatting code clean up.

--- packages/graph-explorer-proxy-server/node-server.js
+++ packages/graph-explorer-proxy-server/node-server.js
@@ -11,12 +11,13 @@ const path = require("path");
 const { fromNodeProviderChain } = require("@aws-sdk/credential-providers");
 const aws4 = require("aws4");
 
-
 const getCredentials = async () => {
   try {
     const credentialProvider = fromNodeProviderChain();
     const results = await credentialProvider();
-    console.log("IAM credentials were found in provider chain and will be used to sign requests");
+    console.log(
+      "IAM credentials were found in provider chain and will be used to sign requests"
+    );
     return results;
   } catch (e) {
     console.error("No IAM credentials found in provider chain", e);
@@ -24,15 +25,16 @@ const getCredentials = async () => {
 };
 
 async function getIAMHeaders(options) {
-
   let creds = await getCredentials();
   if (creds === undefined) {
-    throw new Error("IAM is enabled but credentials cannot be found on the credential provider chain.")
+    throw new Error(
+      "IAM is enabled but credentials cannot be found on the credential provider chain."
+    );
   }
   const headers = aws4.sign(options, {
-      accessKeyId: creds.accessKeyId,
-      secretAccessKey: creds.secretAccessKey,
-      sessionToken: creds.sessionToken
+    accessKeyId: creds.accessKeyId,
+    secretAccessKey: creds.secretAccessKey,
+    sessionToken: creds.sessionToken,
   }).headers;
 
   return headers;
@@ -41,51 +43,47 @@ async function getIAMHeaders(options) {
 dotenv.config({ path: "../graph-explorer/.env" });
 
 (async () => {
-
   app.use(cors());
 
-  app.use(process.env.GRAPH_EXP_ENV_ROOT_FOLDER, express.static(path.join(__dirname, "../graph-explorer/dist")));
+  app.use(
+    process.env.GRAPH_EXP_ENV_ROOT_FOLDER,
+    express.static(path.join(__dirname, "../graph-explorer/dist"))
+  );
 
-  const delay = (ms) => new Promise((resolve) => setTimeout(() => resolve(), ms));
+  const delay = (ms) =>
+    new Promise((resolve) => setTimeout(() => resolve(), ms));
 
-  async function retryFetch (
-    url,
-    headers,
-    retries = 1,
-    retryDelay = 10000
-  ) {
-
+  async function retryFetch(url, headers, retries = 1, retryDelay = 10000) {
     if (headers["aws-neptune-region"]) {
       await getIAMHeaders({
         host: url.hostname,
         port: url.port,
-        path: url.pathname+url.search,
+        path: url.pathname + url.search,
         service: "neptune-db",
-        region: headers["aws-neptune-region"]
-      }).then(data => {  
+        region: headers["aws-neptune-region"],
+      }).then((data) => {
         headers = data;
       });
-    } 
+    }
 
     return new Promise((resolve, reject) => {
-      
       urlString = url.href;
       method = "GET";
 
       console.log("url: ", urlString);
-      console.log
+      console.log;
 
       const wrapper = (n) => {
         fetch(url.href, { headers: headers })
-          .then( async res => {
+          .then(async (res) => {
             if (!res.ok) {
               result = await res.json();
               console.log("Bad response: ", res.statusText);
               console.log("Error message: ", result);
-              const error = res.status
+              const error = res.status;
               return Promise.reject(error);
             } else {
-              console.log("Successful response: "+res.statusText);
+              console.log("Successful response: " + res.statusText);
               resolve(res);
             }
           })
@@ -101,22 +99,19 @@ dotenv.config({ path: "../graph-explorer/.env" });
 
       wrapper(retries);
     });
-  };
+  }
 
   app.get("/sparql", async (req, res, next) => {
-    let response;
-    let data;
     try {
-      response = await retryFetch(
+      const response = await retryFetch(
         new URL(
           `${req.headers["graph-db-connection-url"]}/sparql?query=` +
-          encodeURIComponent(req.query.query) +
-          "&format=json"
-        ), 
+            encodeURIComponent(req.query.query) +
+            "&format=json"
+        ),
         req.headers
-      ).then((res) => res)
-
-      data = await response.json();
+      );
+      const data = await response.json();
       res.send(data);
     } catch (error) {
       next(error);
@@ -125,18 +120,15 @@ dotenv.config({ path: "../graph-explorer/.env" });
   });
 
   app.get("/", async (req, res, next) => {
-    let response;
-    let data;
     try {
-      response = await retryFetch(
+      const response = await retryFetch(
         new URL(
-          `${req.headers["graph-db-connection-url"]}/?gremlin=` + 
-          encodeURIComponent(req.query.gremlin)
-        ), 
+          `${req.headers["graph-db-connection-url"]}/?gremlin=` +
+            encodeURIComponent(req.query.gremlin)
+        ),
         req.headers
-      ).then((res) => res)
-
-      data = await response.json();
+      );
+      const data = await response.json();
       res.send(data);
     } catch (error) {
       next(error);
@@ -145,15 +137,14 @@ dotenv.config({ path: "../graph-explorer/.env" });
   });
 
   app.get("/pg/statistics/summary", async (req, res, next) => {
-    let response;
-    let data;
     try {
-      response = await retryFetch(
-        new URL(`${req.headers["graph-db-connection-url"]}/pg/statistics/summary`), 
+      const response = await retryFetch(
+        new URL(
+          `${req.headers["graph-db-connection-url"]}/pg/statistics/summary`
+        ),
         req.headers
-      ).then((res) => res)
-
-      data = await response.json();
+      );
+      const data = await response.json();
       res.send(data);
     } catch (error) {
       next(error);
@@ -162,17 +153,13 @@ dotenv.config({ path: "../graph-explorer/.env" });
   });
 
   app.get("/rdf/statistics/summary", async (req, res, next) => {
-    let response;
-    let data;
-    let url = new URL(`${req.headers["graph-db-connection-url"]}/rdf/statistics/summary`)
-    let headers = req.headers
     try {
-      response = await retryFetch(
-        url, 
-        headers
-      ).then((res) => res)
-
-      data = await response.json();
+      const url = new URL(
+        `${req.headers["graph-db-connection-url"]}/rdf/statistics/summary`
+      );
+      const headers = req.headers;
+      const response = await retryFetch(url, headers);
+      const data = await response.json();
       res.send(data);
     } catch (error) {
       next(error);
@@ -180,7 +167,12 @@ dotenv.config({ path: "../graph-explorer/.env" });
     }
   });
 
-  if (process.env.PROXY_SERVER_HTTPS_CONNECTION != "false" && fs.existsSync("../graph-explorer-proxy-server/cert-info/server.key") && fs.existsSync("../graph-explorer-proxy-server/cert-info/server.crt")) {
+  // Start the server on port 80 or 443 (if HTTPS is enabled)
+  if (
+    process.env.PROXY_SERVER_HTTPS_CONNECTION != "false" &&
+    fs.existsSync("../graph-explorer-proxy-server/cert-info/server.key") &&
+    fs.existsSync("../graph-explorer-proxy-server/cert-info/server.crt")
+  ) {
     https
       .createServer(
         {
