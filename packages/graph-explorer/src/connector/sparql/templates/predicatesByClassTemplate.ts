001c744402bb7c6348a0d62fdfa43f4441fc749d
cubeddu <juan.cubeddu@gmail.com>
2023-09-29 15:47:46 -0500

    updated query to match previous slow query response

--- packages/graph-explorer/src/connector/sparql/templates/predicatesByClassTemplate.ts
+++ packages/graph-explorer/src/connector/sparql/templates/predicatesByClassTemplate.ts
@@ -1,18 +1,19 @@
 // Return all predicates which are connected from the given class
 const predicatesByClassTemplate = (props: { class: string }) => {
   return `
-    SELECT DISTINCT ?pred ?object
+    SELECT ?pred (SAMPLE(?object) as ?sample)
     WHERE {
       {
         SELECT ?subject
         WHERE {
-          ?subject a <${props.class}>;
+          ?subject a <${props.class}>.
         }
         LIMIT 1
       }
       ?subject ?pred ?object.
       FILTER(!isBlank(?object) && isLiteral(?object))
     }
+    GROUP BY ?pred
   `;
 };
 
