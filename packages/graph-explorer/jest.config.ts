c9a56af8ac20bfc9dc5a7de3bd8f601883276539
Kris McGinnes <kmcginnes@me.com>
2024-05-28 18:24:17 -0500

    Enable TSX compile in Jest

--- packages/graph-explorer/jest.config.ts
+++ packages/graph-explorer/jest.config.ts
@@ -4,11 +4,16 @@ const config: Config = {
   displayName: "graph-explorer",
   verbose: true,
   preset: "ts-jest",
-  testEnvironment: "node",
-  moduleFileExtensions: ["js", "ts", "tsx", "json"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "<rootDir>/../tsconfig.json",
+      },
+    ],
+  },
   rootDir: "src",
-  testRegex: ".test.ts$",
-  setupFilesAfterEnv: ["<rootDir>/utils/testing/setupTests.ts"],
   transformIgnorePatterns: [
     "node_modules/(?!(swiper|dom7)/)",
     "node_modules/(?!(react-dnd-html5-backend)/)",
--- packages/graph-explorer/src/setupTests.ts
+++ packages/graph-explorer/src/setupTests.ts
@@ -1,3 +1,22 @@
+/*
+ * Global Jest setup for all tests
+ */
+
 // Sets up `fetch` for JSDom environment.
 // https://github.com/jsdom/jsdom/issues/1724#issuecomment-720727999
 import "whatwg-fetch";
+
+// Mock the env module
+jest.mock("./utils/env", () => {
+  return {
+    DEV: true,
+    PROD: false,
+  };
+});
+
+// Mock localforage
+jest.mock("localforage", () => ({
+  config: jest.fn(),
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+}));
