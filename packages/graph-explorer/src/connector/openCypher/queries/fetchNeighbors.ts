5323b930d36fb1d304f57de3806ccc8706dd9caa
Kris McGinnes <kmcginnes@me.com>
2024-06-07 17:46:18 -0500

    Remove extraneous openCypher fetch (#431)
    * Remove extraneous openCypher fetch
    
    * Add changelog entry

--- packages/graph-explorer/src/connector/openCypher/queries/fetchNeighbors.ts
+++ packages/graph-explorer/src/connector/openCypher/queries/fetchNeighbors.ts
@@ -5,7 +5,6 @@ import type {
 import mapApiEdge from "../mappers/mapApiEdge";
 import mapApiVertex from "../mappers/mapApiVertex";
 import oneHopTemplate from "../templates/oneHopTemplate";
-import oneHopTemplateEdges from "../templates/oneHopTemplateEdges";
 import type { OCEdge, OCVertex } from "../types";
 import { OpenCypherFetch } from "../types";
 
@@ -24,8 +23,6 @@ type RawOneHopRequest = {
   ];
 };
 
-type NeighborsRequestEdge = NeighborsRequest & { edgeIds: string[] };
-
 const fetchNeighbors = async (
   openCypherFetch: OpenCypherFetch,
   req: NeighborsRequest
@@ -38,20 +35,8 @@ const fetchNeighbors = async (
     mapApiEdge(edgeInfo.edge, edgeInfo.sourceType[0], edgeInfo.targetType[0])
   );
 
-  const edgeReq: NeighborsRequestEdge = {
-    ...req,
-    edgeIds: [
-      ...oneHopData.results[0].eObjects.map(edgeInfo => edgeInfo.edge["~id"]),
-    ],
-  };
-
-  const openCypherTemplateEdges = oneHopTemplateEdges(edgeReq);
-  const oneHopEdgeData = await openCypherFetch<RawOneHopRequest>(
-    openCypherTemplateEdges
-  );
-
   const vertices: NeighborsResponse["vertices"] =
-    oneHopEdgeData.results[0].vObjects.map(vertex => mapApiVertex(vertex));
+    oneHopData.results[0].vObjects.map(vertex => mapApiVertex(vertex));
 
   return {
     vertices,
