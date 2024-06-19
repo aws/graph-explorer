01c61f12ebf5119230d9d76be617f233f8fe17fe
Kris McGinnes <kmcginnes@me.com>
2024-06-19 12:05:18 -0500

    Remove unnecessary mapping

--- packages/graph-explorer/src/modules/CreateConnection/CreateConnection.tsx
+++ packages/graph-explorer/src/modules/CreateConnection/CreateConnection.tsx
@@ -63,10 +63,6 @@ const CreateConnection = ({
     ? {
         ...(existingConfig.connection || {}),
         name: existingConfig.displayLabel || existingConfig.id,
-        url: existingConfig.connection?.url,
-        queryEngine: existingConfig.connection?.queryEngine,
-        fetchTimeoutMs: existingConfig.connection?.fetchTimeoutMs,
-        serviceType: existingConfig.connection?.serviceType,
       }
     : undefined;
 
