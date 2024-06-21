df8afba8e23cf7a4e3e5bfe1bdd4e691756708a0
Kris McGinnes <kmcginnes@me.com>
2024-06-21 16:04:46 -0500

    Normalize in tests

--- packages/graph-explorer/src/connector/gremlin/templates/verticesSchemaTemplate.test.ts
+++ packages/graph-explorer/src/connector/gremlin/templates/verticesSchemaTemplate.test.ts
@@ -1,14 +1,17 @@
 import verticesSchemaTemplate from "./verticesSchemaTemplate";
+import { normalizeWithNoSpace as normalize } from "../../../utils/testing";
 
 describe("Gremlin > verticesSchemaTemplate", () => {
   it("Should return a template with the projection of each type", () => {
     const template = verticesSchemaTemplate({ types: ["airport", "country"] });
 
-    expect(template).toBe(
-      'g.V().project("airport","country")' +
-        '.by(V().hasLabel("airport").limit(1))' +
-        '.by(V().hasLabel("country").limit(1))' +
-        ".limit(1)"
+    expect(normalize(template)).toBe(
+      normalize(`
+        g.V().project("airport","country")
+          .by(V().hasLabel("airport").limit(1))
+          .by(V().hasLabel("country").limit(1))
+          .limit(1)
+      `)
     );
   });
 });
