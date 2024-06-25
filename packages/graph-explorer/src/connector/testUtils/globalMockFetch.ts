dd614d3ea912ccc6dbcb3fdb2352e3d852d240aa
Kris McGinnes <kmcginnes@me.com>
2024-06-24 20:25:46 -0500

    Fix tests

--- packages/graph-explorer/src/connector/testUtils/globalMockFetch.ts
+++ packages/graph-explorer/src/connector/testUtils/globalMockFetch.ts
@@ -8,9 +8,9 @@ const RESPONSES_FILES_MAP: Record<string, string> = {
   "7062d2e": `${GREMLIN}/edges-labels-and-counts.json`,
   "35be2501": `${GREMLIN}/should-return-1-random-node.json`,
   "54fa1494": `${GREMLIN}/should-return-airports-whose-code-matches-with-SFA.json`,
-  e9e93b2: `${GREMLIN}/should-return-all-neighbors-from-node-2018.json`,
+  "749bfe6f": `${GREMLIN}/should-return-all-neighbors-from-node-2018.json`,
   "37e14b1": `${GREMLIN}/should-return-all-neighbors-from-node-2018-counts.json`,
-  "5698a21a": `${GREMLIN}/should-return-filtered-neighbors-from-node-2018.json`,
+  "3332c010": `${GREMLIN}/should-return-filtered-neighbors-from-node-2018.json`,
   "40a4690b": `${GREMLIN}/should-return-filtered-neighbors-from-node-2018-counts.json`,
   "59bc2d43": `${GREMLIN}/should-return-neighbors-counts-for-node-123.json`,
 };
