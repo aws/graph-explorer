251e66d702865cedf2b83f07345094b0b0f11162
Kris McGinnes <kmcginnes@me.com>
2024-07-14 15:34:41 -0500

    Turn off rules that would cause many changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -52,7 +52,15 @@ export default [
 
       // TypeScript
       "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-unsafe-member-access": "off",
+      "@typescript-eslint/no-unsafe-assignment": "off",
+      "@typescript-eslint/no-unsafe-call": "off",
+      "@typescript-eslint/no-unsafe-return": "off",
+      "@typescript-eslint/no-unsafe-argument": "off",
+      "@typescript-eslint/no-unused-expressions": "off",
+      "@typescript-eslint/unbound-method": "off",
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/no-misused-promises": "off",
       "@typescript-eslint/no-unused-vars": [
         "error",
         {
