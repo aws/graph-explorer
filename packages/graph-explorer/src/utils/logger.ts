e6ca97a1e110f8ff5841d2aaf3b85ab6ef6e7070
Kris McGinnes <kmcginnes@me.com>
2024-08-09 15:50:10 -0500

    Fix logger param output

--- packages/graph-explorer/src/utils/logger.ts
+++ packages/graph-explorer/src/utils/logger.ts
@@ -20,7 +20,7 @@ export default {
       return;
     }
     optionalParams.length > 0
-      ? console.debug(message, optionalParams)
+      ? console.debug(message, ...optionalParams)
       : console.debug(message);
   },
   /** Calls `console.log` if the app is running in DEV mode. */
@@ -29,19 +29,19 @@ export default {
       return;
     }
     optionalParams.length > 0
-      ? console.log(message, optionalParams)
+      ? console.log(message, ...optionalParams)
       : console.log(message);
   },
   /** Calls `console.warn`. */
   warn(message?: any, ...optionalParams: any[]) {
     optionalParams.length > 0
-      ? console.warn(message, optionalParams)
+      ? console.warn(message, ...optionalParams)
       : console.warn(message);
   },
   /** Calls `console.error`. */
   error(message?: any, ...optionalParams: any[]) {
     optionalParams.length > 0
-      ? console.error(message, optionalParams)
+      ? console.error(message, ...optionalParams)
       : console.error(message);
   },
 };
