547b6c86f790135a4d4fb02a407c322aec2812cc
Kris McGinnes <kmcginnes@me.com>
2024-08-18 11:13:21 -0500

    Remove custom fontSize in Tailwind theme

--- packages/graph-explorer/tailwind.config.ts
+++ packages/graph-explorer/tailwind.config.ts
@@ -93,17 +93,6 @@ export default {
         "sans-serif",
       ],
     },
-    fontSize: {
-      xs: "0.75rem",
-      sm: "0.875rem",
-      base: "1rem",
-      lg: "1.125rem",
-      xl: "1.25rem",
-      "2xl": "1.5rem",
-      "3xl": "1.75rem",
-      "4xl": "2rem",
-      "5xl": "2.5rem",
-    },
     fontWeight: {
       light: "400",
       base: "500",
