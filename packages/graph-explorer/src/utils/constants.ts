9329c962201980b1f8ae587e92ac472c643fbce9
Kris McGinnes <kmcginnes@me.com>
2024-09-04 09:23:43 -0500

    Fix 404 issue on reload (#581)
    * Use custom RELOAD_URL constant
    
    * Use BASE_URL instead of GRAPH_EXP_ENV_ROOT_FOLDER
    
    * Fix formatting

--- packages/graph-explorer/src/utils/constants.ts
+++ packages/graph-explorer/src/utils/constants.ts
@@ -5,3 +5,9 @@ export const DEFAULT_CONCURRENT_REQUESTS_LIMIT = 10;
 
 /** The string "Graph Explorer". */
 export const APP_NAME = "Graph Explorer";
+
+/** The root URL for the app used for reloading fresh. */
+export const RELOAD_URL =
+  import.meta.env.BASE_URL.substring(-1) !== "/"
+    ? import.meta.env.BASE_URL + "/"
+    : import.meta.env.BASE_URL;
