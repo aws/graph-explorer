949e6341f20f2e3971ce3d5d85ea4bf97200d16b
Kris McGinnes <kmcginnes@me.com>
2024-09-12 12:24:37 -0500

    Fix use of date-fns

--- packages/graph-explorer/src/utils/formatDate.ts
+++ packages/graph-explorer/src/utils/formatDate.ts
@@ -1,4 +1,4 @@
-import dateFormat from "date-fns/format";
+import { format as dateFormat } from "date-fns";
 
 const formatDate = (date: Date, format = "MMM dd yyyy, h:mm a") => {
   return dateFormat(date, format);
