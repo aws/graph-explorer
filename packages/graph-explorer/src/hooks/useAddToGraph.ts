ca7e08a2c9ed9cf09da42d18c570fe6dd685a834
Kris McGinnes <kmcginnes@me.com>
2025-01-08 16:10:15 -0600

    Update useAddToGraph for efficiency

--- packages/graph-explorer/src/hooks/useAddToGraph.ts
+++ packages/graph-explorer/src/hooks/useAddToGraph.ts
@@ -1,16 +1,18 @@
 import { Edge, Vertex } from "@/types/entities";
-import useEntities from "./useEntities";
 import { toNodeMap } from "@/core/StateProvider/nodes";
 import { toEdgeMap } from "@/core/StateProvider/edges";
+import entitiesSelector from "@/core/StateProvider/entitiesSelector";
+import { useCallback } from "react";
+import { useSetRecoilState } from "recoil";
 
 /** Returns a callback that adds an array of nodes and edges to the graph. */
 export function useAddToGraph(...entitiesToAdd: (Vertex | Edge)[]) {
-  const [, setEntities] = useEntities();
+  const setEntities = useSetRecoilState(entitiesSelector);
 
-  const nodes = entitiesToAdd.filter(e => e.entityType === "vertex");
-  const edges = entitiesToAdd.filter(e => e.entityType === "edge");
+  return useCallback(() => {
+    const nodes = entitiesToAdd.filter(e => e.entityType === "vertex");
+    const edges = entitiesToAdd.filter(e => e.entityType === "edge");
 
-  return () => {
     if (nodes.length === 0 && edges.length === 0) {
       return;
     }
@@ -19,5 +21,5 @@ export function useAddToGraph(...entitiesToAdd: (Vertex | Edge)[]) {
       nodes: toNodeMap(nodes),
       edges: toEdgeMap(edges),
     });
-  };
+  }, [entitiesToAdd, setEntities]);
 }
