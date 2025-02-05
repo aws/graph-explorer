50883d9a9b7938033249ecae9e24c3ca9cff2d6b
Kris McGinnes <kmcginnes@me.com>
2025-02-05 11:05:37 -0600

    Tweak ImportantBlock styles

--- packages/graph-explorer/src/components/Typography.tsx
+++ packages/graph-explorer/src/components/Typography.tsx
@@ -45,7 +45,7 @@ export function ImportantBlock({
   return (
     <div
       className={cn(
-        "border-info-main bg-info-light/20 text-info-dark rounded border-l-4 px-4 py-1",
+        "border-info-main bg-info-light/20 text-info-dark rounded-md border-l-4 px-4 py-2",
         className
       )}
       {...props}
