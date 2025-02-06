225c2243b53bf45332e7ba4d88dc9c9aa62b1ed4
Kris McGinnes <kmcginnes@me.com>
2025-02-06 12:08:44 -0600

    Add file saving helper to use native save dialog

--- packages/graph-explorer/src/utils/fileData.ts
+++ packages/graph-explorer/src/utils/fileData.ts
@@ -1,3 +1,5 @@
+import { saveAs } from "file-saver";
+
 export function toJsonFileData(input: object) {
   return new Blob([JSON.stringify(input)], {
     type: "application/json",
@@ -13,4 +15,30 @@ export function toCsvFileData(input: string) {
 export async function fromFileToJson(blob: Blob) {
   const textContents = await blob.text();
   return JSON.parse(textContents) as unknown;
+}
+
+/**
+ * Saves a file using the native file save dialog if possible.
+ *
+ * If the browser does not support the native file save dialog, it will fall back
+ * to using the `file-saver` library.
+ */
+export async function saveFile(file: Blob, defaultFileName: string) {
+  if (!("showSaveFilePicker" in window)) {
+    saveAs(file, defaultFileName);
+  }
+
+  const fileHandle = await window.showSaveFilePicker({
+    suggestedName: defaultFileName,
+    types: [
+      {
+        description: "JSON",
+        accept: { "application/json": [".json"] },
+      },
+    ],
+  });
+
+  const writable = await fileHandle.createWritable();
+  await writable.write(file);
+  await writable.close();
 }
