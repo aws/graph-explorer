d752733f95222dfbe3d2cdd3b62d0961a9066a67
Kris McGinnes <kmcginnes@me.com>
2025-02-07 16:22:16 -0600

    Fix issues in remove from graph

--- packages/graph-explorer/src/hooks/useRemoveFromGraph.test.ts
+++ packages/graph-explorer/src/hooks/useRemoveFromGraph.test.ts
@@ -1,41 +1,69 @@
-/* eslint-disable @typescript-eslint/require-await */
 import {
   createRandomEdge,
   createRandomVertex,
   renderHookWithRecoilRoot,
 } from "@/utils/testing";
-import useEntities from "./useEntities";
 import { act } from "react";
-import { nodesAtom, toNodeMap } from "@/core/StateProvider/nodes";
+import {
+  nodesAtom,
+  nodesFilteredIdsAtom,
+  nodesOutOfFocusIdsAtom,
+  nodesSelectedIdsAtom,
+  toNodeMap,
+} from "@/core/StateProvider/nodes";
 import {
   useClearGraph,
   useRemoveEdgeFromGraph,
   useRemoveNodeFromGraph,
 } from "./useRemoveFromGraph";
-import { edgesAtom, toEdgeMap } from "@/core/StateProvider/edges";
+import {
+  edgesAtom,
+  edgesFilteredIdsAtom,
+  edgesOutOfFocusIdsAtom,
+  edgesSelectedIdsAtom,
+  toEdgeMap,
+} from "@/core/StateProvider/edges";
 import { createArray } from "@shared/utils/testing";
+import { useRecoilValue } from "recoil";
+import { waitFor } from "@testing-library/react";
 
 test("should remove one node", async () => {
   const vertex = createRandomVertex();
   const noise = createArray(10, createRandomVertex);
+  const idsOfNoise = noise.map(n => n.id);
   const { result } = renderHookWithRecoilRoot(
     () => {
       const callback = useRemoveNodeFromGraph(vertex.id);
-      const [entities] = useEntities();
+      const nodes = useRecoilValue(nodesAtom);
+      const selected = useRecoilValue(nodesSelectedIdsAtom);
+      const outOfFocus = useRecoilValue(nodesOutOfFocusIdsAtom);
+      const filtered = useRecoilValue(nodesFilteredIdsAtom);
 
-      return { callback, entities };
+      return { callback, nodes, selected, outOfFocus, filtered };
     },
     snapshot => {
       snapshot.set(nodesAtom, toNodeMap([vertex, ...noise]));
+      snapshot.set(nodesSelectedIdsAtom, new Set([vertex.id, ...idsOfNoise]));
+      snapshot.set(nodesOutOfFocusIdsAtom, new Set([vertex.id, ...idsOfNoise]));
+      snapshot.set(nodesFilteredIdsAtom, new Set([vertex.id, ...idsOfNoise]));
     }
   );
 
-  await act(async () => {
+  act(() => {
     result.current.callback();
   });
 
-  expect(result.current.entities.nodes.has(vertex.id)).toBeFalsy();
-  expect(result.current.entities.nodes.size).toBe(noise.length);
+  await waitFor(() => {
+    expect(result.current.nodes.has(vertex.id)).toBeFalsy();
+    expect(result.current.selected.has(vertex.id)).toBeFalsy();
+    expect(result.current.outOfFocus.has(vertex.id)).toBeFalsy();
+    expect(result.current.filtered.has(vertex.id)).toBeFalsy();
+
+    expect(result.current.nodes.size).toBe(noise.length);
+    expect(result.current.selected.size).toBe(noise.length);
+    expect(result.current.outOfFocus.size).toBe(noise.length);
+    expect(result.current.filtered.size).toBe(noise.length);
+  });
 });
 
 test("should remove one edge", async () => {
@@ -47,23 +75,83 @@ test("should remove one edge", async () => {
   const { result } = renderHookWithRecoilRoot(
     () => {
       const callback = useRemoveEdgeFromGraph(edge1.id);
-      const [entities] = useEntities();
+      const edges = useRecoilValue(edgesAtom);
+      const selected = useRecoilValue(edgesSelectedIdsAtom);
+      const outOfFocus = useRecoilValue(edgesOutOfFocusIdsAtom);
+      const filtered = useRecoilValue(edgesFilteredIdsAtom);
 
-      return { callback, entities };
+      return { callback, edges, selected, outOfFocus, filtered };
     },
     snapshot => {
       snapshot.set(nodesAtom, toNodeMap([node1, node2]));
       snapshot.set(edgesAtom, toEdgeMap([edge1, edge2]));
+      snapshot.set(edgesSelectedIdsAtom, new Set([edge1.id, edge2.id]));
+      snapshot.set(edgesOutOfFocusIdsAtom, new Set([edge1.id, edge2.id]));
+      snapshot.set(edgesFilteredIdsAtom, new Set([edge1.id, edge2.id]));
     }
   );
 
-  await act(async () => {
+  act(() => {
     result.current.callback();
   });
 
-  expect(result.current.entities.edges.has(edge1.id)).toBeFalsy();
-  expect(result.current.entities.edges.has(edge2.id)).toBeTruthy();
-  expect(result.current.entities.nodes.size).toBe(2);
+  await waitFor(() => {
+    expect(result.current.edges.has(edge1.id)).toBeFalsy();
+    expect(result.current.edges.has(edge2.id)).toBeTruthy();
+    expect(result.current.edges.has(edge1.id)).toBeFalsy();
+    expect(result.current.selected.has(edge1.id)).toBeFalsy();
+    expect(result.current.selected.has(edge2.id)).toBeTruthy();
+    expect(result.current.outOfFocus.has(edge1.id)).toBeFalsy();
+    expect(result.current.outOfFocus.has(edge2.id)).toBeTruthy();
+    expect(result.current.filtered.has(edge1.id)).toBeFalsy();
+    expect(result.current.filtered.has(edge2.id)).toBeTruthy();
+  });
+});
+
+test("should remove associate edges when a node is removed", async () => {
+  const node1 = createRandomVertex();
+  const node2 = createRandomVertex();
+  const edge1 = createRandomEdge(node1, node2);
+  const edge2 = createRandomEdge(node2, node1);
+
+  const { result } = renderHookWithRecoilRoot(
+    () => {
+      const callback = useRemoveNodeFromGraph(node2.id);
+
+      const edges = useRecoilValue(edgesAtom);
+      const edgesSelected = useRecoilValue(edgesSelectedIdsAtom);
+      const edgesOutOfFocus = useRecoilValue(edgesOutOfFocusIdsAtom);
+      const edgesFiltered = useRecoilValue(edgesFilteredIdsAtom);
+
+      return {
+        callback,
+        edges,
+        edgesSelected,
+        edgesOutOfFocus,
+        edgesFiltered,
+      };
+    },
+    snapshot => {
+      snapshot.set(nodesAtom, toNodeMap([node1, node2]));
+
+      snapshot.set(edgesAtom, toEdgeMap([edge1, edge2]));
+      snapshot.set(edgesSelectedIdsAtom, new Set([edge1.id, edge2.id]));
+      snapshot.set(edgesOutOfFocusIdsAtom, new Set([edge1.id, edge2.id]));
+      snapshot.set(edgesFilteredIdsAtom, new Set([edge1.id, edge2.id]));
+    }
+  );
+
+  act(() => {
+    result.current.callback();
+  });
+
+  await waitFor(() => {
+    // Should remove all edges since they were associated with the removed node
+    expect(result.current.edges.size).toBe(0);
+    expect(result.current.edgesSelected.size).toBe(0);
+    expect(result.current.edgesOutOfFocus.size).toBe(0);
+    expect(result.current.edgesFiltered.size).toBe(0);
+  });
 });
 
 test("should remove all nodes and edges", async () => {
@@ -75,9 +163,30 @@ test("should remove all nodes and edges", async () => {
   const { result } = renderHookWithRecoilRoot(
     () => {
       const callback = useClearGraph();
-      const [entities] = useEntities();
 
-      return { callback, entities };
+      // Nodes
+      const nodes = useRecoilValue(nodesAtom);
+      const nodesSelected = useRecoilValue(nodesSelectedIdsAtom);
+      const nodesOutOfFocus = useRecoilValue(nodesOutOfFocusIdsAtom);
+      const nodesFiltered = useRecoilValue(nodesFilteredIdsAtom);
+
+      // Edges
+      const edges = useRecoilValue(edgesAtom);
+      const edgesSelected = useRecoilValue(edgesSelectedIdsAtom);
+      const edgesOutOfFocus = useRecoilValue(edgesOutOfFocusIdsAtom);
+      const edgesFiltered = useRecoilValue(edgesFilteredIdsAtom);
+
+      return {
+        callback,
+        nodes,
+        nodesSelected,
+        nodesOutOfFocus,
+        nodesFiltered,
+        edges,
+        edgesSelected,
+        edgesOutOfFocus,
+        edgesFiltered,
+      };
     },
     snapshot => {
       snapshot.set(nodesAtom, toNodeMap([node1, node2]));
@@ -85,10 +194,18 @@ test("should remove all nodes and edges", async () => {
     }
   );
 
-  await act(async () => {
+  act(() => {
     result.current.callback();
   });
 
-  expect(result.current.entities.nodes.size).toBe(0);
-  expect(result.current.entities.edges.size).toBe(0);
+  await waitFor(() => {
+    expect(result.current.nodes.size).toBe(0);
+    expect(result.current.nodesSelected.size).toBe(0);
+    expect(result.current.nodesOutOfFocus.size).toBe(0);
+    expect(result.current.nodesFiltered.size).toBe(0);
+    expect(result.current.edges.size).toBe(0);
+    expect(result.current.edgesSelected.size).toBe(0);
+    expect(result.current.edgesOutOfFocus.size).toBe(0);
+    expect(result.current.edgesFiltered.size).toBe(0);
+  });
 });
--- packages/graph-explorer/src/hooks/useRemoveFromGraph.ts
+++ packages/graph-explorer/src/hooks/useRemoveFromGraph.ts
@@ -1,10 +1,29 @@
-import { EdgeId, VertexId } from "@/core";
-import entitiesSelector from "@/core/StateProvider/entitiesSelector";
-import { useSetRecoilState } from "recoil";
-import { useCallback } from "react";
+import {
+  EdgeId,
+  edgesAtom,
+  edgesFilteredIdsAtom,
+  edgesOutOfFocusIdsAtom,
+  edgesSelectedIdsAtom,
+  nodesAtom,
+  nodesFilteredIdsAtom,
+  nodesOutOfFocusIdsAtom,
+  nodesSelectedIdsAtom,
+  VertexId,
+} from "@/core";
+import { useRecoilValue, useResetRecoilState, useSetRecoilState } from "recoil";
+import { startTransition, useCallback } from "react";
 
 export function useRemoveFromGraph() {
-  const setEntities = useSetRecoilState(entitiesSelector);
+  const setVertices = useSetRecoilState(nodesAtom);
+  const setEdges = useSetRecoilState(edgesAtom);
+  const setSelectedVertices = useSetRecoilState(nodesSelectedIdsAtom);
+  const setSelectedEdges = useSetRecoilState(edgesSelectedIdsAtom);
+  const setOutOfFocusVertices = useSetRecoilState(nodesOutOfFocusIdsAtom);
+  const setOutOfFocusEdges = useSetRecoilState(edgesOutOfFocusIdsAtom);
+  const setFilteredVertices = useSetRecoilState(nodesFilteredIdsAtom);
+  const setFilteredEdges = useSetRecoilState(edgesFilteredIdsAtom);
+
+  const allEdges = useRecoilValue(edgesAtom);
 
   return useCallback(
     (entities: { vertices?: VertexId[]; edges?: EdgeId[] }) => {
@@ -16,16 +35,56 @@ export function useRemoveFromGraph() {
         return;
       }
 
-      setEntities(prev => ({
-        nodes: new Map(
-          prev.nodes.entries().filter(([id]) => !vertices.has(id))
-        ),
-        edges: new Map(prev.edges.entries().filter(([id]) => !edges.has(id))),
-        forceSet: true,
-      }));
+      // Find associated edges for removed vertices
+      const associatedEdges = new Set(
+        allEdges
+          .entries()
+          .filter(
+            ([_id, edge]) =>
+              vertices.has(edge.source) || vertices.has(edge.target)
+          )
+          .map(([id]) => id)
+      );
+      const edgesToRemove = edges.union(associatedEdges);
+
+      startTransition(() => {
+        // Remove vertices
+        if (vertices.size > 0) {
+          setVertices(prev => deleteFromMap(prev, vertices));
+          setSelectedVertices(prev => prev.difference(vertices));
+          setOutOfFocusVertices(prev => prev.difference(vertices));
+          setFilteredVertices(prev => prev.difference(vertices));
+        }
+
+        // Remove edges
+        if (edgesToRemove.size > 0) {
+          setEdges(prev => deleteFromMap(prev, edgesToRemove));
+          setSelectedEdges(prev => prev.difference(edgesToRemove));
+          setOutOfFocusEdges(prev => prev.difference(edgesToRemove));
+          setFilteredEdges(prev => prev.difference(edgesToRemove));
+        }
+      });
     },
-    [setEntities]
+    [
+      allEdges,
+      setVertices,
+      setSelectedVertices,
+      setOutOfFocusVertices,
+      setFilteredVertices,
+      setEdges,
+      setSelectedEdges,
+      setOutOfFocusEdges,
+      setFilteredEdges,
+    ]
   );
+}
+
+function deleteFromMap<Key, Value>(map: Map<Key, Value>, keys: Set<Key>) {
+  const copiedMap = new Map(map);
+  keys.forEach(id => {
+    copiedMap.delete(id);
+  });
+  return copiedMap;
 }
 
 export function useRemoveNodeFromGraph(nodeId: VertexId) {
@@ -45,13 +104,32 @@ export function useRemoveEdgeFromGraph(edgeId: EdgeId) {
 }
 
 export function useClearGraph() {
-  const setEntities = useSetRecoilState(entitiesSelector);
+  const resetVertices = useResetRecoilState(nodesAtom);
+  const resetEdges = useResetRecoilState(edgesAtom);
+  const resetSelectedVertices = useResetRecoilState(nodesSelectedIdsAtom);
+  const resetSelectedEdges = useResetRecoilState(edgesSelectedIdsAtom);
+  const resetOutOfFocusVertices = useResetRecoilState(nodesOutOfFocusIdsAtom);
+  const resetOutOfFocusEdges = useResetRecoilState(edgesOutOfFocusIdsAtom);
+  const resetFilteredVertices = useResetRecoilState(nodesFilteredIdsAtom);
+  const resetFilteredEdges = useResetRecoilState(edgesFilteredIdsAtom);
 
   return useCallback(() => {
-    setEntities({
-      nodes: new Map(),
-      edges: new Map(),
-      forceSet: true,
-    });
-  }, [setEntities]);
+    resetVertices();
+    resetEdges();
+    resetSelectedVertices();
+    resetSelectedEdges();
+    resetOutOfFocusVertices();
+    resetOutOfFocusEdges();
+    resetFilteredVertices();
+    resetFilteredEdges();
+  }, [
+    resetVertices,
+    resetEdges,
+    resetSelectedVertices,
+    resetSelectedEdges,
+    resetOutOfFocusVertices,
+    resetOutOfFocusEdges,
+    resetFilteredVertices,
+    resetFilteredEdges,
+  ]);
 }
