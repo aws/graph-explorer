a4a2bdfd80ae7f06c2f4f1e02b8ea0978ae283a5
Kris McGinnes <kmcginnes@me.com>
2025-02-10 13:22:14 -0600

    Update useAddToGraph

--- packages/graph-explorer/src/hooks/useAddToGraph.test.ts
+++ packages/graph-explorer/src/hooks/useAddToGraph.test.ts
@@ -1,30 +1,40 @@
-/* eslint-disable @typescript-eslint/require-await */
 import {
   createRandomEdge,
   createRandomEntities,
   createRandomVertex,
+  DbState,
   renderHookWithRecoilRoot,
 } from "@/utils/testing";
 import { useAddToGraph } from "./useAddToGraph";
-import useEntities from "./useEntities";
 import { act } from "react";
 import { nodesAtom, toNodeMap } from "@/core/StateProvider/nodes";
+import { useRecoilValue } from "recoil";
+import { edgesAtom, EdgeTypeConfig, VertexTypeConfig } from "@/core";
+import { waitFor } from "@testing-library/react";
+import {
+  activeSchemaSelector,
+  extractConfigFromEntity,
+} from "@/core/StateProvider/schema";
 
 test("should add one node", async () => {
   const vertex = createRandomVertex();
+
   const { result } = renderHookWithRecoilRoot(() => {
     const callback = useAddToGraph();
-    const [entities] = useEntities();
+    const vertices = useRecoilValue(nodesAtom);
+    const edges = useRecoilValue(edgesAtom);
 
-    return { callback, entities };
+    return { callback, vertices, edges };
   });
 
-  await act(async () => {
+  act(() => {
     result.current.callback({ vertices: [vertex] });
   });
 
-  const actual = result.current.entities.nodes.get(vertex.id);
-  expect(actual).toEqual(vertex);
+  await waitFor(() => {
+    const actual = result.current.vertices.get(vertex.id);
+    expect(actual).toEqual(vertex);
+  });
 });
 
 test("should add one edge", async () => {
@@ -35,44 +45,189 @@ test("should add one edge", async () => {
   const { result } = renderHookWithRecoilRoot(
     () => {
       const callback = useAddToGraph();
-      const [entities] = useEntities();
+      const vertices = useRecoilValue(nodesAtom);
+      const edges = useRecoilValue(edgesAtom);
 
-      return { callback, entities };
+      return { callback, vertices, edges };
     },
     snapshot => {
       snapshot.set(nodesAtom, toNodeMap([node1, node2]));
     }
   );
 
-  await act(async () => {
+  act(() => {
     result.current.callback({ edges: [edge] });
   });
 
-  const actual = result.current.entities.edges.get(edge.id);
-  expect(actual).toEqual(edge);
+  await waitFor(() => {
+    const actual = result.current.edges.get(edge.id);
+    expect(actual).toEqual(edge);
+  });
 });
 
 test("should add multiple nodes and edges", async () => {
   const randomEntities = createRandomEntities();
+
   const { result } = renderHookWithRecoilRoot(() => {
     const callback = useAddToGraph();
-    const [entities] = useEntities();
+    const vertices = useRecoilValue(nodesAtom);
+    const edges = useRecoilValue(edgesAtom);
 
-    return { callback, entities };
+    return { callback, vertices, edges };
   });
 
-  await act(async () => {
+  act(() => {
     result.current.callback({
       vertices: [...randomEntities.nodes.values()],
       edges: [...randomEntities.edges.values()],
     });
   });
 
-  const actualNodes = result.current.entities.nodes.values().toArray();
-  const expectedNodes = randomEntities.nodes.values().toArray();
-  expect(actualNodes).toEqual(expectedNodes);
+  await waitFor(() => {
+    const actualNodes = result.current.vertices.values().toArray();
+    const expectedNodes = randomEntities.nodes.values().toArray();
+    expect(actualNodes).toEqual(expectedNodes);
 
-  const actualEdges = result.current.entities.edges.values().toArray();
-  const expectedEdges = randomEntities.edges.values().toArray();
-  expect(actualEdges).toEqual(expectedEdges);
+    const actualEdges = result.current.edges.values().toArray();
+    const expectedEdges = randomEntities.edges.values().toArray();
+    expect(actualEdges).toEqual(expectedEdges);
+  });
+});
+
+test("should update schema when adding a node", async () => {
+  const vertex = createRandomVertex();
+  const expectedVertexType = extractConfigFromEntity(vertex);
+  const dbState = new DbState();
+
+  const { result } = renderHookWithRecoilRoot(
+    () => {
+      const callback = useAddToGraph();
+      const vertices = useRecoilValue(nodesAtom);
+      const edges = useRecoilValue(edgesAtom);
+      const schema = useRecoilValue(activeSchemaSelector);
+
+      return { callback, vertices, edges, schema };
+    },
+    snapshot => dbState.applyTo(snapshot)
+  );
+
+  act(() => {
+    result.current.callback({ vertices: [vertex] });
+  });
+
+  await waitFor(() => {
+    const vtConfig = result.current.schema?.vertices.find(
+      v => v.type === vertex.type
+    );
+    expect(vtConfig).toEqual(expectedVertexType);
+  });
+});
+
+test("should update schema when adding an edge", async () => {
+  const node1 = createRandomVertex();
+  const node2 = createRandomVertex();
+  const edge = createRandomEdge(node1, node2);
+  const expectedEdgeType = extractConfigFromEntity(edge);
+  const dbState = new DbState();
+  dbState.addVertexToGraph(node1);
+  dbState.addVertexToGraph(node2);
+
+  const { result } = renderHookWithRecoilRoot(
+    () => {
+      const callback = useAddToGraph();
+      const vertices = useRecoilValue(nodesAtom);
+      const edges = useRecoilValue(edgesAtom);
+      const schema = useRecoilValue(activeSchemaSelector);
+
+      return { callback, vertices, edges, schema };
+    },
+    snapshot => dbState.applyTo(snapshot)
+  );
+
+  act(() => {
+    result.current.callback({ edges: [edge] });
+  });
+
+  await waitFor(() => {
+    const etConfig = result.current.schema?.edges.find(
+      e => e.type === edge.type
+    );
+    expect(etConfig).toEqual(expectedEdgeType);
+  });
+});
+
+test("should add missing attributes to the schema when adding a node", async () => {
+  const vertex = createRandomVertex();
+  const expectedVertexType = extractConfigFromEntity(vertex);
+  const dbState = new DbState();
+
+  // Clear out the attributes in the initial schema
+  const initialVtConfig: VertexTypeConfig = {
+    ...expectedVertexType,
+    attributes: [],
+  };
+  dbState.activeSchema.vertices.push(initialVtConfig);
+
+  const { result } = renderHookWithRecoilRoot(
+    () => {
+      const callback = useAddToGraph();
+      const vertices = useRecoilValue(nodesAtom);
+      const edges = useRecoilValue(edgesAtom);
+      const schema = useRecoilValue(activeSchemaSelector);
+
+      return { callback, vertices, edges, schema };
+    },
+    snapshot => dbState.applyTo(snapshot)
+  );
+
+  act(() => {
+    result.current.callback({ vertices: [vertex] });
+  });
+
+  await waitFor(() => {
+    const vtConfig = result.current.schema?.vertices.find(
+      v => v.type === vertex.type
+    );
+    expect(vtConfig).toEqual(expectedVertexType);
+  });
+});
+
+test("should ad missing attributes to the schema when adding an edge", async () => {
+  const node1 = createRandomVertex();
+  const node2 = createRandomVertex();
+  const edge = createRandomEdge(node1, node2);
+  const expectedEdgeType = extractConfigFromEntity(edge);
+  const dbState = new DbState();
+  dbState.addVertexToGraph(node1);
+  dbState.addVertexToGraph(node2);
+
+  // Clear out the attributes in the initial schema
+  const initialEtConfig: EdgeTypeConfig = {
+    ...expectedEdgeType,
+    attributes: [],
+  };
+  dbState.activeSchema.edges.push(initialEtConfig);
+
+  const { result } = renderHookWithRecoilRoot(
+    () => {
+      const callback = useAddToGraph();
+      const vertices = useRecoilValue(nodesAtom);
+      const edges = useRecoilValue(edgesAtom);
+      const schema = useRecoilValue(activeSchemaSelector);
+
+      return { callback, vertices, edges, schema };
+    },
+    snapshot => dbState.applyTo(snapshot)
+  );
+
+  act(() => {
+    result.current.callback({ edges: [edge] });
+  });
+
+  await waitFor(() => {
+    const etConfig = result.current.schema?.edges.find(
+      e => e.type === edge.type
+    );
+    expect(etConfig).toEqual(expectedEdgeType);
+  });
 });
--- packages/graph-explorer/src/utils/testing/DbState.ts
+++ packages/graph-explorer/src/utils/testing/DbState.ts
@@ -1,4 +1,15 @@
-import { Vertex } from "@/core";
+import {
+  Edge,
+  EdgeId,
+  edgesAtom,
+  edgesFilteredIdsAtom,
+  edgesTypesFilteredAtom,
+  nodesFilteredIdsAtom,
+  nodesTypesFilteredAtom,
+  toEdgeMap,
+  Vertex,
+  VertexId,
+} from "@/core";
 import {
   activeConfigurationAtom,
   configurationAtom,
@@ -20,7 +31,14 @@ import { createRandomSchema, createRandomRawConfiguration } from "./randomData";
 export class DbState {
   activeSchema: Schema;
   activeConfig: RawConfiguration;
+
   vertices: Vertex[] = [];
+  filteredVertices: Set<VertexId> = new Set();
+  filteredVertexTypes: Set<string> = new Set();
+
+  edges: Edge[] = [];
+  filteredEdges: Set<EdgeId> = new Set();
+  filteredEdgeTypes: Set<string> = new Set();
 
   constructor() {
     this.activeSchema = createRandomSchema();
@@ -36,8 +54,30 @@ export class DbState {
     this.activeSchema.vertices.push(extractConfigFromEntity(vertex));
   }
 
+  filterVertex(vertexId: VertexId) {
+    this.filteredVertices.add(vertexId);
+  }
+
+  filterVertexType(vertexType: string) {
+    this.filteredVertexTypes.add(vertexType);
+  }
+
+  addEdgeToGraph(edge: Edge) {
+    this.edges.push(edge);
+    this.activeSchema.edges.push(extractConfigFromEntity(edge));
+  }
+
+  filterEdge(edgeId: EdgeId) {
+    this.filteredEdges.add(edgeId);
+  }
+
+  filterEdgeType(edgeType: string) {
+    this.filteredEdgeTypes.add(edgeType);
+  }
+
   /** Applies the state to the given Recoil snapshot. */
   applyTo(snapshot: MutableSnapshot) {
+    // Config
     snapshot.set(
       configurationAtom,
       new Map([[this.activeConfig.id, this.activeConfig]])
@@ -47,6 +87,15 @@ export class DbState {
       new Map([[this.activeConfig.id, this.activeSchema]])
     );
     snapshot.set(activeConfigurationAtom, this.activeConfig.id);
+
+    // Vertices
     snapshot.set(nodesAtom, toNodeMap(this.vertices));
+    snapshot.set(nodesFilteredIdsAtom, this.filteredVertices);
+    snapshot.set(nodesTypesFilteredAtom, this.filteredVertexTypes);
+
+    // Edges
+    snapshot.set(edgesAtom, toEdgeMap(this.edges));
+    snapshot.set(edgesFilteredIdsAtom, this.filteredEdges);
+    snapshot.set(edgesTypesFilteredAtom, this.filteredEdgeTypes);
   }
 }
