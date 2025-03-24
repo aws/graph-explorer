ead5cf38d38a3418e6dc15874a0a63fc053dc409
Kris McGinnes <kmcginnes@me.com>
2025-03-24 18:44:18 -0500

    Use mono font and add more height

--- packages/graph-explorer/src/modules/SearchSidebar/QuerySearchTabContent.tsx
+++ packages/graph-explorer/src/modules/SearchSidebar/QuerySearchTabContent.tsx
@@ -69,7 +69,7 @@ export function QuerySearchTabContent() {
                   <TextArea
                     {...field}
                     aria-label="Query"
-                    className="h-full w-full"
+                    className="h-full min-h-[5lh] w-full font-mono"
                     onKeyDown={e => {
                       if (e.key === "Enter" && (e.metaKey || e.ctrlKey)) {
                         e.preventDefault();
