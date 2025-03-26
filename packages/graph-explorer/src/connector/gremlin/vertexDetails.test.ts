1958a0ec0cbb0a888d4cf350460eb6ce31bcf7f1
Kris McGinnes <kmcginnes@me.com>
2025-03-26 17:36:37 -0500

    Add test for gremlin vertexDetails

--- packages/graph-explorer/src/connector/gremlin/vertexDetails.test.ts
+++ packages/graph-explorer/src/connector/gremlin/vertexDetails.test.ts
@@ -18,4 +18,20 @@ describe("vertexDetails", () => {
 
     expect(result.vertex).toEqual(vertex);
   });
+
+  it("should not be fragment if the response does not include the properties", async () => {
+    const vertex = createRandomVertex();
+    vertex.attributes = {};
+    vertex.__isFragment = true;
+    const response = createGremlinResponseFromVertex(vertex);
+    const mockFetch = vi
+      .fn()
+      .mockImplementation(() => Promise.resolve(response));
+
+    const result = await vertexDetails(mockFetch, {
+      vertexId: vertex.id,
+    });
+
+    expect(result.vertex?.__isFragment).toBe(false);
+  });
 });
