5368eb12748516bac3e5a2656b1d0ac18debc5d3
Kris McGinnes <kmcginnes@me.com>
2025-03-26 17:29:22 -0500

    Move to separate file and add tests

--- packages/graph-explorer/src/core/entities.ts
+++ packages/graph-explorer/src/core/entities.ts
@@ -1,5 +1,4 @@
 import { Branded } from "@/utils";
-import { createEdgeId, createVertexId } from "./entityIdType";
 
 export type EdgeId = Branded<string | number, "EdgeId">;
 export type VertexId = Branded<string | number, "VertexId">;
@@ -99,66 +98,3 @@ export type Entities = {
   nodes: Map<VertexId, Vertex>;
   edges: Map<EdgeId, Edge>;
 };
-
-type CreateEntityAttributeOptions =
-  | Map<string, string | number>
-  | Record<string, string | number>;
-
-type CreateVertexOptions = {
-  id: string | number;
-  types: string[];
-  attributes?: CreateEntityAttributeOptions;
-  isBlankNode?: boolean;
-};
-
-export function createVertex(options: CreateVertexOptions): Vertex {
-  return {
-    entityType: "vertex",
-    id: createVertexId(options.id),
-    type: options.types[0] ?? "",
-    types: options.types,
-    attributes:
-      options.attributes != null ? createAttributes(options.attributes) : {},
-    __isFragment: options.attributes == null,
-    __isBlank: options.isBlankNode ?? false,
-  };
-}
-
-export function createEdge(options: {
-  id: string | number;
-  type: string;
-  source: CreateVertexOptions;
-  target: CreateVertexOptions;
-  attributes?: CreateEntityAttributeOptions;
-}): Edge {
-  const source = createVertex(options.source);
-  const target = createVertex(options.target);
-  return {
-    entityType: "edge",
-    id: createEdgeId(options.id),
-    type: options.type,
-    source: source.id,
-    sourceTypes: source.types,
-    target: target.id,
-    targetTypes: target.types,
-    attributes:
-      options.attributes != null ? createAttributes(options.attributes) : {},
-    __isFragment: options.attributes == null,
-  };
-}
-
-function createAttributes(
-  attributes: CreateEntityAttributeOptions
-): Vertex["attributes"] {
-  if (attributes instanceof Map) {
-    return attributes.entries().reduce(
-      (prev, [key, value]) => {
-        prev[key] = value;
-        return prev;
-      },
-      {} as Vertex["attributes"]
-    );
-  }
-
-  return attributes;
-}
--- packages/graph-explorer/src/core/index.ts
+++ packages/graph-explorer/src/core/index.ts
@@ -3,6 +3,7 @@ export * from "./ThemeProvider";
 export * from "./featureFlags";
 export * from "./StateProvider";
 export * from "./connector";
+export * from "./createEntities";
 export * from "./entityIdType";
 export * from "./entities";
 export * from "./renderedEntities";
