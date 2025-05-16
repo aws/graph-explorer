8031e31a2f21a4506440352f47e0e57cfea3fcf2
Kris McGinnes <kmcginnes@me.com>
2025-05-16 17:36:52 -0500

    Update dialog in edge style

--- packages/graph-explorer/src/modules/EdgesStyling/EdgeStyleDialog.tsx
+++ packages/graph-explorer/src/modules/EdgesStyling/EdgeStyleDialog.tsx
@@ -1,7 +1,18 @@
-import { Modal } from "@mantine/core";
-import { Button, InputField, SelectField } from "@/components";
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  InputField,
+  SelectField,
+} from "@/components";
 import ColorInput from "@/components/ColorInput/ColorInput";
-import { useDisplayEdgeTypeConfig, useWithTheme } from "@/core";
+import { useDisplayEdgeTypeConfig } from "@/core";
 import {
   ArrowStyle,
   LineStyle,
@@ -13,44 +24,29 @@ import {
   TARGET_ARROW_STYLE_OPTIONS,
 } from "./arrowsStyling";
 import { LINE_STYLE_OPTIONS } from "./lineStyling";
-import modalDefaultStyles from "./SingleEdgeStylingModal.style";
 import { RESERVED_TYPES_PROPERTY } from "@/utils";
 import { atom, useAtom } from "jotai";
 
 export const customizeEdgeTypeAtom = atom<string | undefined>(undefined);
 
 export default function EdgeStyleDialog() {
-  const styleWithTheme = useWithTheme();
-
   const [customizeEdgeType, setCustomizeEdgeType] = useAtom(
     customizeEdgeTypeAtom
   );
 
   return (
-    <Modal
-      opened={Boolean(customizeEdgeType)}
-      onClose={() => setCustomizeEdgeType(undefined)}
-      centered={true}
-      size="auto"
-      title={
-        customizeEdgeType ? <DialogTitle edgeType={customizeEdgeType} /> : null
-      }
-      className={styleWithTheme(modalDefaultStyles)}
-      overlayProps={{
-        backgroundOpacity: 0.1,
+    <Dialog
+      open={Boolean(customizeEdgeType)}
+      onOpenChange={opened => {
+        if (!opened) {
+          setCustomizeEdgeType(undefined);
+        }
       }}
     >
-      {customizeEdgeType ? <Content edgeType={customizeEdgeType} /> : null}
-    </Modal>
-  );
-}
-
-function DialogTitle({ edgeType }: { edgeType: string }) {
-  const displayConfig = useDisplayEdgeTypeConfig(edgeType);
-  return (
-    <div>
-      Customize <strong>{displayConfig.displayLabel}</strong>
-    </div>
+      <DialogContent>
+        {customizeEdgeType ? <Content edgeType={customizeEdgeType} /> : null}
+      </DialogContent>
+    </Dialog>
   );
 }
 
@@ -75,129 +71,148 @@ function Content({ edgeType }: { edgeType: string }) {
   })();
 
   return (
-    <div className="modal-container">
-      <div>
-        <p>Display Attributes</p>
-        <div className="attrs-container">
-          <SelectField
-            label="Display Name Attribute"
-            labelPlacement="inner"
-            value={displayConfig.displayNameAttribute}
-            onValueChange={value =>
-              setEdgeStyle({ displayNameAttribute: value })
-            }
-            options={selectOptions}
-          />
+    <>
+      <DialogHeader>
+        <DialogTitle>Customize Edge Style</DialogTitle>
+        <DialogDescription>
+          Customize styles for edge type {displayConfig.displayLabel}
+        </DialogDescription>
+      </DialogHeader>
+      <DialogBody>
+        <div className="space-y-4">
+          <div className="font-medium leading-none">Display Attributes</div>
+          <div className="flex justify-between gap-4">
+            <SelectField
+              label="Display Name Attribute"
+              labelPlacement="inner"
+              value={displayConfig.displayNameAttribute}
+              onValueChange={value =>
+                setEdgeStyle({ displayNameAttribute: value })
+              }
+              options={selectOptions}
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <p>Label Styling</p>
-        <div className="attrs-container">
-          <ColorInput
-            label="Color"
-            labelPlacement="inner"
-            color={edgeStyle?.labelColor || "#17457b"}
-            onChange={(color: string) => setEdgeStyle({ labelColor: color })}
-          />
-          <InputField
-            label="Background Opacity"
-            labelPlacement="inner"
-            type="number"
-            min={0}
-            max={1}
-            step={0.1}
-            value={edgeStyle?.labelBackgroundOpacity ?? 0.7}
-            onChange={(value: number) =>
-              setEdgeStyle({ labelBackgroundOpacity: value })
-            }
-          />
+        <div className="space-y-4">
+          <div className="font-medium leading-none">Label Styling</div>
+          <div className="grid grid-cols-2 gap-4">
+            <ColorInput
+              label="Color"
+              labelPlacement="inner"
+              color={edgeStyle?.labelColor || "#17457b"}
+              onChange={(color: string) => setEdgeStyle({ labelColor: color })}
+              className="grow"
+            />
+            <InputField
+              label="Background Opacity"
+              labelPlacement="inner"
+              type="number"
+              min={0}
+              max={1}
+              step={0.1}
+              value={edgeStyle?.labelBackgroundOpacity ?? 0.7}
+              onChange={(value: number) =>
+                setEdgeStyle({ labelBackgroundOpacity: value })
+              }
+              className="grow"
+            />
+          </div>
+          <div className="grid grid-cols-3 gap-4">
+            <ColorInput
+              label="Border Color"
+              labelPlacement="inner"
+              color={edgeStyle?.labelBorderColor || "#17457b"}
+              onChange={(color: string) =>
+                setEdgeStyle({ labelBorderColor: color })
+              }
+            />
+            <InputField
+              label="Border Width"
+              labelPlacement="inner"
+              type="number"
+              min={0}
+              value={edgeStyle?.labelBorderWidth ?? 0}
+              onChange={(value: number) =>
+                setEdgeStyle({ labelBorderWidth: value })
+              }
+            />
+            <SelectField
+              label="Border Style"
+              labelPlacement="inner"
+              value={edgeStyle?.labelBorderStyle || "solid"}
+              onValueChange={value =>
+                setEdgeStyle({
+                  labelBorderStyle: value as LineStyle,
+                })
+              }
+              options={LINE_STYLE_OPTIONS}
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <div className="attrs-container">
-          <ColorInput
-            label="Border Color"
-            labelPlacement="inner"
-            color={edgeStyle?.labelBorderColor || "#17457b"}
-            onChange={(color: string) =>
-              setEdgeStyle({ labelBorderColor: color })
-            }
-          />
-          <InputField
-            label="Border Width"
-            labelPlacement="inner"
-            type="number"
-            min={0}
-            value={edgeStyle?.labelBorderWidth ?? 0}
-            onChange={(value: number) =>
-              setEdgeStyle({ labelBorderWidth: value })
-            }
-          />
-          <SelectField
-            label="Border Style"
-            labelPlacement="inner"
-            value={edgeStyle?.labelBorderStyle || "solid"}
-            onValueChange={value =>
-              setEdgeStyle({ labelBorderStyle: value as LineStyle })
-            }
-            options={LINE_STYLE_OPTIONS}
-          />
+        <div className="space-y-4">
+          <div className="font-medium leading-none">Line Styling</div>
+          <div className="grid grid-cols-3 gap-4">
+            <ColorInput
+              label="Color"
+              labelPlacement="inner"
+              color={edgeStyle?.lineColor || "#b3b3b3"}
+              onChange={(color: string) => setEdgeStyle({ lineColor: color })}
+            />
+            <InputField
+              label="Thickness"
+              labelPlacement="inner"
+              type="number"
+              min={1}
+              value={edgeStyle?.lineThickness || 2}
+              onChange={(value: number) =>
+                setEdgeStyle({ lineThickness: value })
+              }
+            />
+            <SelectField
+              label="Style"
+              labelPlacement="inner"
+              value={edgeStyle?.lineStyle || "solid"}
+              onValueChange={value =>
+                setEdgeStyle({ lineStyle: value as LineStyle })
+              }
+              options={LINE_STYLE_OPTIONS}
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <p>Line Styling</p>
-        <div className="attrs-container">
-          <ColorInput
-            label="Color"
-            labelPlacement="inner"
-            color={edgeStyle?.lineColor || "#b3b3b3"}
-            onChange={(color: string) => setEdgeStyle({ lineColor: color })}
-          />
-          <InputField
-            label="Thickness"
-            labelPlacement="inner"
-            type="number"
-            min={1}
-            value={edgeStyle?.lineThickness || 2}
-            onChange={(value: number) => setEdgeStyle({ lineThickness: value })}
-          />
-          <SelectField
-            label="Style"
-            labelPlacement="inner"
-            value={edgeStyle?.lineStyle || "solid"}
-            onValueChange={value =>
-              setEdgeStyle({ lineStyle: value as LineStyle })
-            }
-            options={LINE_STYLE_OPTIONS}
-          />
+        <div className="space-y-4">
+          <div className="font-medium leading-none">Arrows Styling</div>
+          <div className="flex justify-between gap-4">
+            <SelectField
+              label="Source"
+              labelPlacement="inner"
+              value={edgeStyle?.sourceArrowStyle || "none"}
+              onValueChange={value =>
+                setEdgeStyle({
+                  sourceArrowStyle: value as ArrowStyle,
+                })
+              }
+              options={SOURCE_ARROW_STYLE_OPTIONS}
+            />
+            <SelectField
+              label="Target"
+              labelPlacement="inner"
+              value={edgeStyle?.targetArrowStyle || "triangle"}
+              onValueChange={value =>
+                setEdgeStyle({
+                  targetArrowStyle: value as ArrowStyle,
+                })
+              }
+              options={TARGET_ARROW_STYLE_OPTIONS}
+            />
+          </div>
         </div>
-      </div>
-      <div>
-        <p>Arrows Styling</p>
-        <div className="attrs-container">
-          <SelectField
-            label="Source"
-            labelPlacement="inner"
-            value={edgeStyle?.sourceArrowStyle || "none"}
-            onValueChange={value =>
-              setEdgeStyle({ sourceArrowStyle: value as ArrowStyle })
-            }
-            options={SOURCE_ARROW_STYLE_OPTIONS}
-          />
-          <SelectField
-            label="Target"
-            labelPlacement="inner"
-            value={edgeStyle?.targetArrowStyle || "triangle"}
-            onValueChange={value =>
-              setEdgeStyle({ targetArrowStyle: value as ArrowStyle })
-            }
-            options={TARGET_ARROW_STYLE_OPTIONS}
-          />
-        </div>
-      </div>
-      <div className="actions">
-        <Button onClick={resetEdgeStyle}>Reset to Default</Button>
-      </div>
-    </div>
+      </DialogBody>
+      <DialogFooter className="sm:justify-between">
+        <Button onPress={resetEdgeStyle}>Reset to Default</Button>
+        <DialogClose asChild>
+          <Button variant="filled">Done</Button>
+        </DialogClose>
+      </DialogFooter>
+    </>
   );
 }
