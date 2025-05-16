139e67733761ea240eea31311b5e0e68ac656251
Kris McGinnes <kmcginnes@me.com>
2025-05-16 16:23:38 -0500

    Move ref forwarding logic

--- packages/graph-explorer/src/components/TextArea.tsx
+++ packages/graph-explorer/src/components/TextArea.tsx
@@ -16,32 +16,31 @@ export interface TextAreaProps
   errorMessage?: string;
 }
 
-export const TextArea = (
-  { ...props }: TextAreaProps,
-  ref: ForwardedRef<HTMLTextAreaElement>
-) => {
-  const { className, validationState, errorMessage } = props;
-  const localRef = useRef<HTMLTextAreaElement>(null);
-  const { inputProps } = useTextField(
-    props,
-    (ref as RefObject<HTMLTextAreaElement | null>) || localRef
-  );
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ ...props }: TextAreaProps, ref: ForwardedRef<HTMLTextAreaElement>) => {
+    const { className, validationState, errorMessage } = props;
+    const localRef = useRef<HTMLTextAreaElement>(null);
+    const { inputProps } = useTextField(
+      props,
+      (ref as RefObject<HTMLTextAreaElement | null>) || localRef
+    );
 
-  return (
-    <div className={cn("space-y-1", className)}>
-      <textarea
-        className={cn(
-          "placeholder:text-text-secondary text-text-primary focus-visible:ring-ring border-divider bg-input-background hover:bg-input-hover aria-invalid:ring-error-main aria-invalid:border-error-main invalid:border-error-main flex w-full rounded-md border px-3 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
-          className
+    return (
+      <div className={cn("space-y-1", className)}>
+        <textarea
+          className={cn(
+            "placeholder:text-text-secondary text-text-primary focus-visible:ring-ring border-divider bg-input-background hover:bg-input-hover aria-invalid:ring-error-main aria-invalid:border-error-main invalid:border-error-main flex w-full rounded-md border px-3 py-2 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+            className
+          )}
+          ref={ref || localRef}
+          {...inputProps}
+        />
+        {validationState === "invalid" && !!errorMessage && (
+          <FormError>{errorMessage}</FormError>
         )}
-        ref={ref || localRef}
-        {...inputProps}
-      />
-      {validationState === "invalid" && !!errorMessage && (
-        <FormError>{errorMessage}</FormError>
-      )}
-    </div>
-  );
-};
+      </div>
+    );
+  }
+);
 
-export default forwardRef<HTMLTextAreaElement, TextAreaProps>(TextArea);
+export default TextArea;
