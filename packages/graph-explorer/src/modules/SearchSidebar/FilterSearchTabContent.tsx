3bbc9e73caa02ded300efe5ab047a25c69c6158c
Kris McGinnes <kmcginnes@me.com>
2025-06-01 18:52:41 -0500

    Execute raw queries as a mutation

--- packages/graph-explorer/src/modules/SearchSidebar/FilterSearchTabContent.tsx
+++ packages/graph-explorer/src/modules/SearchSidebar/FilterSearchTabContent.tsx
@@ -11,9 +11,16 @@ import {
   Label,
   Checkbox,
   Input,
+  LoadingSpinner,
+  PanelEmptyState,
+  PanelError,
+  SearchSadIcon,
 } from "@/components";
 
 import { useTranslations } from "@/hooks";
+import { KeywordSearchResponse } from "@/connector";
+import { UseQueryResult } from "@tanstack/react-query";
+import { useCancelKeywordSearch } from "./useKeywordSearchQuery";
 
 export function FilterSearchTabContent() {
   const t = useTranslations();
@@ -108,7 +115,52 @@ export function FilterSearchTabContent() {
         </div>
       </div>
 
-      <SearchResultsList query={query} />
+      <SearchResultsListContainer query={query} />
     </div>
   );
+}
+
+function SearchResultsListContainer({
+  query,
+}: {
+  query: UseQueryResult<KeywordSearchResponse | null, Error>;
+}) {
+  const cancelAll = useCancelKeywordSearch();
+
+  if (query.isLoading) {
+    return (
+      <PanelEmptyState
+        title="Searching..."
+        subtitle="Looking for matching results"
+        actionLabel="Cancel"
+        onAction={() => cancelAll()}
+        icon={<LoadingSpinner />}
+        className="p-8"
+      />
+    );
+  }
+
+  if (query.isError && !query.data) {
+    return (
+      <PanelError error={query.error} onRetry={query.refetch} className="p-8" />
+    );
+  }
+
+  if (
+    !query.data ||
+    (query.data.vertices.length === 0 &&
+      query.data.edges.length === 0 &&
+      query.data.scalars.length === 0)
+  ) {
+    return (
+      <PanelEmptyState
+        title="No Results"
+        subtitle="Your criteria does not match with any record"
+        icon={<SearchSadIcon />}
+        className="p-8"
+      />
+    );
+  }
+
+  return <SearchResultsList {...query.data} />;
 }
