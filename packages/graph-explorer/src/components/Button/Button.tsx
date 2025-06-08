f0e36911da495199c884d1f3c1e156ff946a9988
Kris McGinnes <kmcginnes@me.com>
2025-06-07 22:13:01 -0500

    Adjust button horizontal padding

--- packages/graph-explorer/src/components/Button/Button.tsx
+++ packages/graph-explorer/src/components/Button/Button.tsx
@@ -18,8 +18,8 @@ export const buttonStyles = cva({
     },
     size: {
       small: "h-8 rounded px-1 text-base [&_svg]:size-5",
-      base: "h-10 rounded-md px-3 text-base [&_svg]:size-5",
-      large: "h-12 rounded-md px-4 text-lg [&_svg]:size-6",
+      base: "h-10 rounded-md px-4 text-base [&_svg]:size-5",
+      large: "h-12 rounded-md px-5 text-lg [&_svg]:size-6",
     },
   },
   compoundVariants: [
