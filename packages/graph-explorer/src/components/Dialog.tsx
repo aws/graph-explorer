945a3b22c995f327a81a9b002232ee4c0c380f62
Kris McGinnes <kmcginnes@me.com>
2025-07-26 15:15:44 -0500

    Update default width and fix subtitle style

--- packages/graph-explorer/src/components/Dialog.tsx
+++ packages/graph-explorer/src/components/Dialog.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { cn } from "@/utils";
 import { XIcon } from "lucide-react";
+import { IconButton } from "./IconButton";
 
 const Dialog = DialogPrimitive.Root;
 
@@ -36,15 +37,19 @@ const DialogContent = React.forwardRef<
       <DialogPrimitive.Content
         ref={ref}
         className={cn(
-          "bg-background-default data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 relative grid max-h-full max-w-lg overflow-y-auto duration-200 sm:rounded-lg",
+          "bg-background-default data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 relative grid max-h-full w-[500px] overflow-y-auto duration-200 sm:rounded-lg",
           className
         )}
         {...props}
       >
         {children}
-        <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm p-2 opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-          <XIcon className="h-5 w-5" />
-          <span className="sr-only">Close</span>
+        <DialogPrimitive.Close asChild className="absolute right-5 top-5">
+          <IconButton
+            icon={<XIcon />}
+            variant="text"
+            size="small"
+            title="Close"
+          />
         </DialogPrimitive.Close>
       </DialogPrimitive.Content>
     </div>
@@ -112,7 +117,7 @@ const DialogDescription = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
-    className={cn("text-muted-foreground text-sm", className)}
+    className={cn("text-text-secondary text-sm", className)}
     {...props}
   />
 ));
