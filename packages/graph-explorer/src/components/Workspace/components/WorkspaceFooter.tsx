46164677b8a6a20ed207f01ff78c28673f0131c7
Kris McGinnes <kmcginnes@me.com>
2025-07-29 11:49:25 -0500

    Remove unused Workspace components

--- packages/graph-explorer/src/components/Workspace/components/WorkspaceFooter.tsx
+++
@@ -1,9 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-const WorkspaceFooter = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
-};
-
-WorkspaceFooter.displayName = "WorkspaceFooter";
-
-export default WorkspaceFooter;
--- packages/graph-explorer/src/components/Workspace/components/WorkspaceNavBar.tsx
+++
@@ -1,9 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-const WorkspaceNavBar = ({ children }: PropsWithChildren) => {
-  return <>{children}</>;
-};
-
-WorkspaceNavBar.displayName = "WorkspaceNavBar";
-
-export default WorkspaceNavBar;
