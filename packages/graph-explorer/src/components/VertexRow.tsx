9dbf2cfcaeb5fa42a0e653bae3c6c8aad4534c6f
Kris McGinnes <kmcginnes@me.com>
2025-08-07 18:09:16 -0500

    Add name paramter to VertexRow and EdgeRow

--- packages/graph-explorer/src/components/VertexRow.tsx
+++ packages/graph-explorer/src/components/VertexRow.tsx
@@ -4,10 +4,24 @@ import { ComponentPropsWithoutRef } from "react";
 import { cn } from "@/utils";
 
 export function VertexRow({
+  name,
   vertex,
   className,
   ...props
-}: { vertex: DisplayVertex } & ComponentPropsWithoutRef<"div">) {
+}: {
+  vertex: DisplayVertex;
+  name?: string | null;
+} & ComponentPropsWithoutRef<"div">) {
+  const title = name ? (
+    <>
+      {name}: {vertex.displayTypes}&nbsp;&rsaquo; {vertex.displayName}
+    </>
+  ) : (
+    <>
+      {vertex.displayTypes}&nbsp;&rsaquo; {vertex.displayName}
+    </>
+  );
+
   return (
     <div
       className={cn("flex flex-row items-center gap-3", className)}
@@ -15,9 +29,7 @@ export function VertexRow({
     >
       <VertexSymbol vertexStyle={vertex.typeConfig.style} />
       <div>
-        <SearchResultTitle>
-          {vertex.displayTypes}&nbsp;&rsaquo; {vertex.displayName}
-        </SearchResultTitle>
+        <SearchResultTitle>{title}</SearchResultTitle>
         <SearchResultSubtitle>{vertex.displayDescription}</SearchResultSubtitle>
       </div>
     </div>
