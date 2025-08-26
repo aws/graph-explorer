491c1391ffdb9110710ec33fdc7378f8b9e5b963
Kris McGinnes <kmcginnes@me.com>
2025-08-26 13:44:41 -0500

    Remove unused memoize

--- packages/graph-explorer/src/utils/index.ts
+++ packages/graph-explorer/src/utils/index.ts
@@ -2,7 +2,6 @@ export { default as formatDate } from "./formatDate";
 export { default as getChildOfType } from "./getChildOfType";
 export { default as getChildrenOfType } from "./getChildrenOfType";
 export { default as groupChildrenByType } from "./groupChildrenByType";
-export { default as memoize } from "./memoize";
 export { default as useClickOutside } from "./useClickOutside";
 export { DEFAULT_SERVICE_TYPE } from "./constants";
 export { default as escapeString } from "./escapeString";
