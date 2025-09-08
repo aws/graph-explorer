f7c19a31804cda0cee0f7f19c47fd6e4af31c97f
David <75678655+dwrth@users.noreply.github.com>
2025-09-08 22:22:53 +0200

    Add confirmation to deleting a connection (#1136)
    * Implement confirmation dialog for deleting connections (#1120)
    
    * Add OpenSSL back to Docker (#1137)
    
    * Add openssl back to docker
    
    * Ensure openSSH is installed
    
    * Bump version to 2.3.1 for patch (#1138)
    
    * Fix expand neighbor query when edge ID is UUID (#1140)
    
    * Update changelog (#1144)
    
    * Adjust styles in DialogFooter (#1147)
    
    * Adjust layout in DialogFooter
    
    * Adjust restricted width styles
    
    * Bump version to 2.4.0 (#1132)
    
    * Remove unused code (#1131)
    
    * Remove unused loading state from Graph
    
    * Remove unused HumanReadableNumberFormatter
    
    * Remove unused set utilities
    
    * Remove unused memoize
    
    * Remove unused makeIconActionCell
    
    * Update LoadConfigButton dialog (#1148)
    
    * Create random string with 10 characters (#1153)
    
    * Adjust dialog contents
    
    ---------
    
    Co-authored-by: David <75678655+David-Werth@users.noreply.github.com>
    Co-authored-by: Kris McGinnes <kmcginnes@me.com>

---
+++ packages/graph-explorer/src/modules/ConnectionDetail/ConnectionDeleteButton.tsx
@@ -0,0 +1,79 @@
+import {
+  DeleteIcon,
+  PanelHeaderActionButton,
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogBody,
+  Paragraph,
+  Button,
+  ErrorIcon,
+  DialogFooter,
+  DialogDescription,
+} from "@/components";
+import { useState } from "react";
+
+export default function ConnectionDeleteButton({
+  connectionName,
+  isSync,
+  deleteActiveConfig,
+  saveCopy,
+}: {
+  connectionName: string;
+  isSync: boolean;
+  deleteActiveConfig: () => void;
+  saveCopy: () => void;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const saveAndDelete = () => {
+    saveCopy();
+    deleteActiveConfig();
+  };
+
+  return (
+    <>
+      <PanelHeaderActionButton
+        label="Delete connection"
+        icon={<DeleteIcon />}
+        color="danger"
+        isDisabled={isSync}
+        onActionClick={() => setIsOpen(true)}
+      />
+      <Dialog open={isOpen} onOpenChange={setIsOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Delete Connection</DialogTitle>
+            <DialogDescription className="sr-only">
+              Confirm the deletion of the connection named {connectionName}
+            </DialogDescription>
+          </DialogHeader>
+          <DialogBody>
+            <div className="flex flex-row items-start gap-8">
+              <div className="py-1">
+                <ErrorIcon className="text-warning-main size-16" />
+              </div>
+              <Paragraph>
+                Are you sure you want to delete the connection,{" "}
+                <strong className="font-bold">{connectionName}</strong>? This
+                cannot be undone.
+              </Paragraph>
+            </div>
+          </DialogBody>
+          <DialogFooter>
+            <Button onPress={() => setIsOpen(false)}>Cancel</Button>
+            <Button onPress={saveAndDelete}>Save a Copy & Delete</Button>
+            <Button
+              onPress={deleteActiveConfig}
+              color="danger"
+              variant="filled"
+            >
+              Delete
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+}
