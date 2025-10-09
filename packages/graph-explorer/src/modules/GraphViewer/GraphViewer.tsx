d21ce4b8c0af2a3e05fc34c08cb7ebcd2ba22cbd
Chase Naples <cnaples79@gmail.com>
2025-10-09 14:39:17 -0400

    Update packages/graph-explorer/src/modules/GraphViewer/GraphViewer.tsx
    Co-authored-by: Kris McGinnes <kris@mcginnes.io>

--- packages/graph-explorer/src/modules/GraphViewer/GraphViewer.tsx
+++ packages/graph-explorer/src/modules/GraphViewer/GraphViewer.tsx
@@ -148,7 +148,7 @@ export default function GraphViewer() {
             />
             <IconButton
               tooltipText="Zoom to Fit"
-              icon={<ExpandIcon />}
+              icon={<FullscreenIcon />}
               variant="text"
               onClick={onFitAllToCanvas}
             />
