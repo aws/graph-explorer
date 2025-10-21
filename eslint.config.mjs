e6b42171a449d7fb6ffb795c2d5e44728b12e978
Kris McGinnes <kmcginnes@me.com>
2025-10-21 11:39:33 -0500

    Add ESLint rule no-import-type-side-effects

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -58,6 +58,7 @@ export default tseslint.config(
 
       // TypeScript
       "@typescript-eslint/switch-exhaustiveness-check": "error",
+      "@typescript-eslint/no-import-type-side-effects": "error",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
