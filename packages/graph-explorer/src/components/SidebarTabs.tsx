f15287de03afc30089d062207aa013a13b3ce28e
Kris McGinnes <kmcginnes@me.com>
2025-10-30 12:29:01 -0500

    ring-3

--- packages/graph-explorer/src/components/SidebarTabs.tsx
+++ packages/graph-explorer/src/components/SidebarTabs.tsx
@@ -36,7 +36,7 @@ const SidebarTabsTrigger = React.forwardRef<
   <TabsPrimitive.Trigger
     ref={ref}
     className={cn(
-      "focus-visible:ring-ring bg-background-default text-text-secondary ring-offset-background-contrast data-[state=active]:border-primary-main data-[state=active]:text-text-primary inline-flex grow items-center justify-center gap-2 border-b-2 border-transparent px-4 py-2 text-base font-bold whitespace-nowrap transition-all focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-5",
+      "focus-visible:ring-ring-3 bg-background-default text-text-secondary ring-offset-background-contrast data-[state=active]:border-primary-main data-[state=active]:text-text-primary inline-flex grow items-center justify-center gap-2 border-b-2 border-transparent px-4 py-2 text-base font-bold whitespace-nowrap transition-all focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:pointer-events-none disabled:opacity-50 [&_svg]:size-5",
       className
     )}
     {...props}
@@ -51,7 +51,7 @@ const SidebarTabsContent = React.forwardRef<
   <TabsPrimitive.Content
     ref={ref}
     className={cn(
-      "ring-offset-background focus-visible:ring-ring h-full overflow-y-auto focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden",
+      "ring-offset-background focus-visible:ring-ring-3 h-full overflow-y-auto focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden",
       className
     )}
     {...props}
