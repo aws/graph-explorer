e64f0e06ca3cc2d97439b3dbd6139f0ecb0d4f2d
Kris McGinnes <kmcginnes@me.com>
2025-11-13 11:22:16 -0600

    Typescript and JavaScript plugins

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,35 +17,45 @@ export default defineConfig(
   {
     ignores: ["**/*.config.{js,ts,mjs}", "**/vitest.workspace.ts"],
   },
-  // Settings
-  { languageOptions: { parserOptions: { ecmaFeatures: { jsx: true } } } },
-  { settings: { react: { version: "18" } } },
-  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+
+  // JavaScript files
+  pluginJs.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+
+  // TypeScript files - all packages
   {
     languageOptions: {
       parserOptions: {
         projectService: true,
-        tsconfigRootDir: import.meta.dirname,
       },
     },
-  },
-  // Plugins
-  pluginJs.configs.recommended,
-  tseslint.configs.recommendedTypeChecked,
-  // General rules
-  {
     rules: {
+      // Disallow use of console.log
       "no-console": ["error", { allow: ["warn", "error"] }],
 
-      // TypeScript
+      // Force all switches to be exhaustive
       "@typescript-eslint/switch-exhaustiveness-check": "error",
+
+      // Ensure imports are marked with type when appropriate
       "@typescript-eslint/consistent-type-imports": [
         "error",
+        { fixStyle: "inline-type-imports" },
+      ],
+
+      // Allow unused vars with modifier
+      "@typescript-eslint/no-unused-vars": [
+        "error",
         {
-          fixStyle: "inline-type-imports",
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
         },
       ],
+
+      // Ensure no type imports contain side effects
       "@typescript-eslint/no-import-type-side-effects": "error",
+
+      // Disable overly strict rules (we should eliminate these over time)
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-assignment": "off",
@@ -56,14 +66,6 @@ export default defineConfig(
       "@typescript-eslint/unbound-method": "off",
       "@typescript-eslint/no-floating-promises": "off",
       "@typescript-eslint/no-misused-promises": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "error",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-          caughtErrorsIgnorePattern: "^_",
-        },
-      ],
     },
   },
 
@@ -74,6 +76,13 @@ export default defineConfig(
       react: reactLint,
       "react-hooks": fixupPluginRules(reactHooks),
       "@tanstack/query": fixupPluginRules(tanstackQueryLint),
+    },
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: { ecmaFeatures: { jsx: true } },
+    },
+    settings: {
+      react: { version: "19" },
     },
     rules: {
       ...reactLint.configs.flat.recommended.rules,
@@ -90,6 +99,14 @@ export default defineConfig(
       "@tanstack/query/exhaustive-deps": "error",
       "@tanstack/query/no-rest-destructuring": "warn",
       "@tanstack/query/stable-query-client": "error",
+    },
+  },
+
+  // Node.js backend files
+  {
+    files: ["packages/graph-explorer-proxy-server/**/*.{ts,js}"],
+    languageOptions: {
+      globals: globals.node,
     },
   },
 
