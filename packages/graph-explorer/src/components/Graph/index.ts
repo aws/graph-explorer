8199ae43020d434ca0e83e7fa7125d2e3838b165
Kris McGinnes <kmcginnes@me.com>
2025-11-24 15:04:58 -0600

    Add GraphContext to contain the graphRef

--- packages/graph-explorer/src/components/Graph/index.ts
+++ packages/graph-explorer/src/components/Graph/index.ts
@@ -3,4 +3,5 @@ export { default as useGraphControls } from "./hooks/useGraphControls";
 export type { GraphProps } from "./Graph";
 export type { Badge } from "./hooks/useRenderBadges";
 export { availableLayoutsConfig } from "./helpers/layoutConfig";
+export { GraphProvider, useGraphRef } from "./GraphContext";
 export { default } from "./Graph";
