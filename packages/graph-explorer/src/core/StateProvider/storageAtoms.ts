3dfa7b4591725989d73cfaba34f7e6ec30e60cd0
Kris McGinnes <kmcginnes@me.com>
2025-12-05 16:19:07 -0600

    Run in parallel

--- packages/graph-explorer/src/core/StateProvider/storageAtoms.ts
+++ packages/graph-explorer/src/core/StateProvider/storageAtoms.ts
@@ -10,59 +10,54 @@ import {
 } from "./index";
 import { atomWithLocalForage } from "./atomWithLocalForage";
 
-export const activeConfigurationAtom =
-  await atomWithLocalForage<ConfigurationId | null>(
-    "active-configuration",
-    null,
-  );
+const [
+  activeConfigurationAtom,
+  configurationAtom,
+  schemaAtom,
+  userStylingAtom,
+  userLayoutAtom,
+  allGraphSessionsAtom,
+  showDebugActionsAtom,
+  allowLoggingDbQueryAtom,
+  defaultNeighborExpansionLimitEnabledAtom,
+  defaultNeighborExpansionLimitAtom,
+] = await Promise.all([
+  atomWithLocalForage<ConfigurationId | null>("active-configuration", null),
+  atomWithLocalForage<Map<ConfigurationId, RawConfiguration>>(
+    "configuration",
+    new Map(),
+  ),
+  /** All the stored schemas */
+  atomWithLocalForage("schema", new Map<string, SchemaInference>()),
+  atomWithLocalForage<UserStyling>("user-styling", {}),
+  atomWithLocalForage("user-layout", defaultUserLayout),
+  /** Stores the graph session data for each connection. */
+  atomWithLocalForage<Map<ConfigurationId, GraphSessionStorageModel>>(
+    "graph-sessions",
+    new Map(),
+  ),
+  /*
+   * General App Settings
+   */
+  /** Shows debug actions in various places around the app. */
+  atomWithLocalForage<boolean>("showDebugActions", false),
+  /** Shows debug actions in various places around the app. */
+  atomWithLocalForage<boolean>("allowLoggingDbQuery", false),
+  /** Setting that enables/disables the default limit for neighbor expansion. */
+  atomWithLocalForage<boolean>("defaultNeighborExpansionLimitEnabled", true),
+  /** Setting that defines the default limit for neighbor expansion. */
+  atomWithLocalForage<number>("defaultNeighborExpansionLimit", 10),
+]);
 
-export const configurationAtom = await atomWithLocalForage<
-  Map<ConfigurationId, RawConfiguration>
->("configuration", new Map());
-
-/** All the stored schemas */
-export const schemaAtom = await atomWithLocalForage(
-  "schema",
-  new Map<string, SchemaInference>(),
-);
-
-export const userStylingAtom = await atomWithLocalForage<UserStyling>(
-  "user-styling",
-  {},
-);
-
-export const userLayoutAtom = await atomWithLocalForage(
-  "user-layout",
-  defaultUserLayout,
-);
-/** Stores the graph session data for each connection. */
-export const allGraphSessionsAtom = await atomWithLocalForage<
-  Map<ConfigurationId, GraphSessionStorageModel>
->("graph-sessions", new Map());
-
-/*
- * General App Settings
- */
-
-/** Shows debug actions in various places around the app. */
-export const showDebugActionsAtom = await atomWithLocalForage<boolean>(
-  "showDebugActions",
-  false,
-);
-
-/** Shows debug actions in various places around the app. */
-export const allowLoggingDbQueryAtom = await atomWithLocalForage<boolean>(
-  "allowLoggingDbQuery",
-  false,
-);
-
-/** Setting that enables/disables the default limit for neighbor expansion. */
-export const defaultNeighborExpansionLimitEnabledAtom =
-  await atomWithLocalForage<boolean>(
-    "defaultNeighborExpansionLimitEnabled",
-    true,
-  );
-
-/** Setting that defines the default limit for neighbor expansion. */
-export const defaultNeighborExpansionLimitAtom =
-  await atomWithLocalForage<number>("defaultNeighborExpansionLimit", 10);
+export {
+  activeConfigurationAtom,
+  configurationAtom,
+  schemaAtom,
+  userStylingAtom,
+  userLayoutAtom,
+  allGraphSessionsAtom,
+  showDebugActionsAtom,
+  allowLoggingDbQueryAtom,
+  defaultNeighborExpansionLimitEnabledAtom,
+  defaultNeighborExpansionLimitAtom,
+};
