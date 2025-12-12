95c0c18ef548ad9a974315f89bb5b797a0255d0a
Kris McGinnes <kmcginnes@me.com>
2025-12-12 15:32:18 -0600

    Update switch style

--- packages/graph-explorer/src/components/Switch.tsx
+++ packages/graph-explorer/src/components/Switch.tsx
@@ -9,7 +9,7 @@ function Switch({
   return (
     <SwitchPrimitives.Root
       className={cn(
-        "focus-visible:ring-offset-background peer focus-visible:ring-primary-main data-[state=checked]:bg-primary-main inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-xs transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50 data-[state=unchecked]:bg-gray-300",
+        "focus-visible:ring-offset-background peer focus-visible:ring-primary-main data-[state=checked]:bg-primary-main data-[state=unchecked]:bg-input-border inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-xs transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-hidden disabled:cursor-not-allowed disabled:opacity-50",
         className,
       )}
       {...props}
